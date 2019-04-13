68055af8d3b0609209fd64d617922707a3b8cead
wangrui0810 <wangrui0810@gmail.com>
2019-04-13 21:34:18 +0800

    多个table

--- zlktqa.py
+++ zlktqa.py
@@ -101,4 +101,4 @@ def getTodayCodeInfo():
 
 # before_request -> 视图函数 -> context_processor
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='127.0.0.1',port=5000)
