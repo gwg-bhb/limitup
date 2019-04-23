688d2da7573ce9a4c9705d865c8d3d033af159e5
wangrui0810 <wangrui0810@gmail.com>
2019-04-23 15:46:21 +0800

    修改密码

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ HOSTNAME = '123.57.81.203'
 PORT     = '3306'
 DATABASE = 'limit_up'
 USERNAME = 'root'
-PASSWORD = '123456'
+PASSWORD = 'L6zK3CCNjVQB'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
